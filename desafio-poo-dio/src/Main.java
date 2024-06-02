3cc22a61bc5980007ac1dbe58db0bee206061119
Valdemir Antonio <gerente.decorarte@hotmail.com>
2024-06-02 09:25:49 -0300

    criaçao de classes

--- desafio-poo-dio/src/Main.java
+++ desafio-poo-dio/src/Main.java
@@ -1,7 +1,19 @@
+import br.com.desafio.dio.dominio.Curso;
+import br.com.desafio.dio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Curso curso = new Curso("Curso de Java", "POO em java", 40);
+        Curso curso2 = new Curso("Curso de Javascript", "DOM na pratica", 20);
 
+        Mentoria mentoria = new Mentoria("Mentoria de Java", "Fluxos em Java", LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
--- desafio-poo-dio/src/br/com/desafio/dio/dominio/Curso.java
+++ desafio-poo-dio/src/br/com/desafio/dio/dominio/Curso.java
@@ -1,4 +1,49 @@
 package br.com.desafio.dio.dominio;
 
+import java.time.LocalDate;
+
 public class Curso {
+    protected String curso;
+    protected String descricao;
+    protected int cargaHoraria;
+
+
+    public Curso(String curso, String descricao, int cargaHoraria) {
+        this.curso = curso;
+        this.descricao = descricao;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "curso='" + curso + '\'' +
+                ", cargaHoraria='" + cargaHoraria + '\'' +
+                '}';
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
 }
